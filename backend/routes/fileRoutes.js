323416efcd1f20b1b9cde7ae74d726f6f23212d4
jantzz <jantz.ang@gmail.com>
2025-03-15 13:25:08 +0800

    changed frontend folder to docs

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 
+//declare absolute path to front end from current dirname
 const fpath = "../../frontend"
 
 const router = express.Router(); 
 
+//routes 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, `${fpath}/login.html`));
 });
